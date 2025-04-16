a60f0cd3af1a25f407cffb0ccf9e274e7cafdde0
Anand <anandsingh14442@gmail.com>
2025-04-16 18:48:50 +0530

    chnage url

--- frontend/src/redux/features/books/booksApi.js
+++ frontend/src/redux/features/books/booksApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import getBaseUrl from '../../../utils/baseUrl';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: `${getBaseUrl()}/api/books/`,
+    baseUrl: `http://localhost:5000/api/books/`,
     credentials: 'include',
     prepareHeaders: (headers) => {
         const token = localStorage.getItem('token');
